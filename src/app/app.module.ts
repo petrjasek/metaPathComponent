5c56ef07ddf282327055959e797a457caecdad4c
Michal Hotovec <michal.hotovec@gmail.com>
2018-05-29 02:47:50 +0200

    sin in pool, swiper in meta-componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {Injector, NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
 
-import { AppComponent } from './app.component';
+import {SwiperConfigInterface} from 'ngx-swiper-wrapper';
+import {SwiperModule} from 'ngx-swiper-wrapper';
+import {SWIPER_CONFIG} from 'ngx-swiper-wrapper';
+import {createCustomElement} from '@angular/elements';
+
+// import {UserPoolComponent} from './user-pool/user-pool.component';
+import {MetapathComponent} from './metapath/metapath.component';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // UserPoolComponent,
+    MetapathComponent
   ],
   imports: [
+    SwiperModule,
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
+  entryComponents: [
+    // UserPoolComponent
+    MetapathComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap() {
+    // const el = createCustomElement(UserPoolComponent, {injector: this.injector});
+    const el = createCustomElement(MetapathComponent, {injector: this.injector});
+    customElements.define('meta-path', el);
+  }
+}
