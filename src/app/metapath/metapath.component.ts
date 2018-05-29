5fc769b72594f26fd84f85e8c5305811e2fa07cf
Michal Hotovec <michal.hotovec@gmail.com>
2018-05-29 10:24:44 +0200

    centering, curve ending

--- src/app/metapath/metapath.component.ts
+++ src/app/metapath/metapath.component.ts
@@ -21,12 +21,27 @@ export class MetapathComponent implements OnInit {
   public containerHeight = 768;
   public ratio = 0.75;
   public minWidth = 1024;
+  public minHeight = 1024;
+  public keepMinSize = false;
 
   public cWidth = 2000;
-  public circle = [200, 400];
-  public points = [1, 2, 3, 4, 5];
+  public circle = [200, 384];
+
+  public points = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14];
+  //public points = [1, 2, 3, 4];
 
   private lastItemX = 0;
+  smooth = 10;
+  stepSmoothDelta = 3;
+  halfYOffset = 384;
+
+  pathPointsSpace = 100;
+  pathStartOffset = 100;
+  pathEndOffset = 100;
+
+  amplitudeY = 205;
+
+  pathString = '';
 
 
   public config: SwiperConfigInterface = {
@@ -46,6 +61,9 @@ export class MetapathComponent implements OnInit {
 
   ngOnInit() {
     this.Resize(window.innerWidth, window.innerHeight);
+
+
+    this.buildPath();
     // console.dir(this.swiperWrapper.directiveRef);
   }
 
@@ -58,20 +76,26 @@ export class MetapathComponent implements OnInit {
     if (!this.updating) {
       this.updating = true;
 
-      let width = winW;
-      if (winW < this.minWidth) {
-        width = this.minWidth;
-      }
+      if (this.keepMinSize) {
+        let width = winW;
+        if (winW < this.minWidth) {
+          width = this.minWidth;
+        }
 
-      const newHeight = width * this.ratio;
+        const newHeight = width * this.ratio;
 
-      if (newHeight <= winH) {
-        this.containerWidth = width;
-        this.containerHeight = Math.round(newHeight);
-        this.Update();
+        if (newHeight <= winH) {
+          this.containerWidth = width;
+          this.containerHeight = Math.round(newHeight);
+          this.Update();
+        } else {
+          this.containerHeight = winH;
+          this.containerWidth = Math.round(winH * 1.33);
+        }
       } else {
-        this.containerHeight = winH;
-        this.containerWidth = Math.round(winH * 1.33);
+        const newHeight = winW * this.ratio;
+        this.containerWidth = winW;
+        this.containerHeight = Math.round(newHeight);
       }
 
       this.Update();
@@ -82,29 +106,78 @@ export class MetapathComponent implements OnInit {
   public Update() {
     setTimeout(() => {
       this.swiperWrapper.directiveRef.update(true);
-      console.log('swupd');
       this.updating = false;
     }, 100);
+  }
+
+  public nodeClick(idx) {
+    console.log('nodeClick' + idx);
   }
 
   public onIndexChange(index: number): void {
   }
 
+  public buildPath() {
+
+    // adjust startoffset for small amont of points to adjust horizontal centering shorter curves
+    const area = (this.points.length - 1) * this.pathPointsSpace;
+    if (area < this.minWidth) {
+      const offset = (this.minWidth - area) / 2;
+      this.pathStartOffset = offset;
+      this.pathEndOffset = offset;
+    }
+
+    // calc min,max Y for vertical center
+    let minY = this.minHeight;
+    let maxY = 0;
+    for (let i = 0; i < this.points.length; i++) {
+      const dx = (((i) / this.smooth) * this.stepSmoothDelta);
+      const dy = -1 * (Math.cos(dx) * this.amplitudeY);
+      minY = Math.min(minY, dy);
+      maxY = Math.max(minY, dy);
+    }
+    // set new half offset for centering different curves
+    this.halfYOffset = this.minHeight / 2 - Math.abs( maxY - minY) / 2 + 60;
+
+    // adjust size of svg
+    this.cWidth = this.pathStartOffset + this.pathEndOffset + ((this.points.length - 1) * this.pathPointsSpace);
+
+    let localPathString = '';
+    for (let i = 0; i < this.points.length; i++) {
+      const __ret = this.buildPoint(i);
+
+      if (i <= 0) {
+        localPathString = localPathString + 'M' + (__ret.x - this.pathStartOffset) + ',' + __ret.y + ', ';
+      }
+      localPathString = localPathString + __ret.svgCurve + __ret.x + ',' + __ret.y + ', ';
+      if (i === this.points.length - 1) {
+        localPathString = localPathString + 'L' + (__ret.x + this.pathEndOffset) + ',' + __ret.y + ', ';
+      }
+      // clean end coma (FF issue)
+      localPathString = localPathString.substr(0, localPathString.length - 2);
+      this.pathString = localPathString;
+    }
+  }
+
+  private buildPoint(i: number) {
+    //  x
+    const x = (i * this.pathPointsSpace) + this.pathStartOffset;
+    //  y
+    const helpx = ((i / this.smooth) * this.stepSmoothDelta);
+    const y = -1 * (Math.cos(helpx) * this.amplitudeY) + this.halfYOffset;
+    const svgCurve = 'L';
+    return {x, y, svgCurve};
+  }
+
   getPosX(idx) {
-    const tx = (idx * 100) + 300;
-    const compensator = 0;
-    // if (idx > 40) {
-    //   compensator = 2;
-    // }
-    // if (idx > 52) {
-    //   compensator = 6;
-    // }
-    // if (idx > 73) {
-    //   compensator = 8;
-    // }
-    this.lastItemX = tx + compensator;
-
-
+    const tx = (idx * this.pathPointsSpace) + this.pathStartOffset;
+    this.lastItemX = tx;
     return this.lastItemX;
+  }
+
+  getPosY(idx) {
+    const x = (((idx) / this.smooth) * this.stepSmoothDelta);
+    const y = -1 * (Math.cos(x) * this.amplitudeY) + this.halfYOffset;
+    return y;
   }
 }
