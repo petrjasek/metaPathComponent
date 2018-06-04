472d81bac400a7037bb345d790f4e097ee52ae61
Michal Hotovec <michal.hotovec@gmail.com>
2018-06-04 12:37:11 +0200

    animation of the 300x300x3

--- src/app/metapath/metapath.component.ts
+++ src/app/metapath/metapath.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {SwiperConfigInterface} from 'ngx-swiper-wrapper';
 
 @Component({
@@ -8,6 +8,10 @@ import {SwiperConfigInterface} from 'ngx-swiper-wrapper';
   encapsulation: ViewEncapsulation.Emulated
 })
 export class MetapathComponent implements OnInit {
+
+  @Input()
+  public playeranim: string;
+
 
   @ViewChild('container') container: ElementRef;
   @ViewChild('swiperWrapper') public swiperWrapper: any;
@@ -27,13 +31,20 @@ export class MetapathComponent implements OnInit {
   public cWidth = 2000;
   public circle = [200, 384];
 
-  public points = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14];
-  //public points = [1, 2, 3, 4];
+  public points = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
+  //public points = [1, 2, 3, 4, 5];
+
+  public path = [];
   private lastItemX = 0;
+  private timer;
+  private walkingSpeed = 300;
+  private isWalking = 0;
+
   smooth = 10;
   stepSmoothDelta = 3;
   halfYOffset = 384;
+  halfYOffsetsLess = [-1, 50, 70, 80, 50, -0, 0, -90, -100, -100, -100];
 
   pathPointsSpace = 100;
   pathStartOffset = 100;
@@ -58,12 +69,15 @@ export class MetapathComponent implements OnInit {
   };
 
   constructor() {
+    // prevent wrong index
+    if (this.currentNodeIndex >= this.points.length) {
+      this.currentNodeIndex = this.points.length - 1;
+    }
   }
 
   ngOnInit() {
     this.Resize(window.innerWidth, window.innerHeight);
-
-
+    console.log(this.playeranim);
     this.buildPath();
     // console.dir(this.swiperWrapper.directiveRef);
   }
@@ -116,6 +130,9 @@ export class MetapathComponent implements OnInit {
 
   public buildPath() {
 
+    // reset path
+    this.path = [];
+
     // adjust startoffset for small amont of points to adjust horizontal centering shorter curves
     const area = (this.points.length - 1) * this.pathPointsSpace;
     if (area < this.minWidth) {
@@ -124,17 +141,12 @@ export class MetapathComponent implements OnInit {
       this.pathEndOffset = offset;
     }
 
-    // calc min,max Y for vertical center
-    let minY = this.minHeight;
-    let maxY = 0;
-    for (let i = 0; i < this.points.length; i++) {
-      const dx = (((i) / this.smooth) * this.stepSmoothDelta);
-      const dy = -1 * (Math.cos(dx) * this.amplitudeY);
-      minY = Math.min(minY, dy);
-      maxY = Math.max(minY, dy);
-    }
+
     // set new half offset for centering different curves
-    this.halfYOffset = this.minHeight / 2 - Math.abs(maxY - minY) / 2 + 60;
+    this.halfYOffset = this.minHeight / 2 - 160;
+    if (this.points.length <= 10) {
+      this.halfYOffset = this.minHeight / 2 + this.halfYOffsetsLess[this.points.length];
+    }
 
     // adjust size of svg
     this.cWidth = this.pathStartOffset + this.pathEndOffset + ((this.points.length - 1) * this.pathPointsSpace);
@@ -142,6 +154,8 @@ export class MetapathComponent implements OnInit {
     let localPathString = '';
     for (let i = 0; i < this.points.length; i++) {
       const __ret = this.buildPoint(i);
+
+      this.path.push(__ret);
 
       if (i <= 0) {
         localPathString = localPathString + 'M' + (__ret.x - this.pathStartOffset) + ',' + __ret.y + ', ';
@@ -154,6 +168,8 @@ export class MetapathComponent implements OnInit {
       localPathString = localPathString.substr(0, localPathString.length - 2);
       this.pathString = localPathString;
     }
+
+    console.dir(this.path);
   }
 
   private buildPoint(i: number) {
@@ -190,6 +206,25 @@ export class MetapathComponent implements OnInit {
     return 'node-next';
   }
 
+  getPlayerPosition(idx) {
+    return {
+      x: this.path[idx].x,
+      y: this.path[idx].y
+    };
+  }
+
+  getPlayerGroupTransform(idx, dir) {
+    const pos = this.getPlayerPosition(idx);
+    let css = 'translate(' + (pos.x - 150) + ',' + (pos.y - 250) + ')';
+
+    if (dir < 0) {
+      css = 'translate(' + (pos.x + 150) + ',' + (pos.y - 250) + ')  scale(-1, 1)';
+    }
+
+    return css;
+  }
+
+
   goToPoint(dir) {
     const newIndex = this.currentNodeIndex + dir;
     if (newIndex >= 0 && newIndex < this.points.length) {
@@ -197,11 +232,32 @@ export class MetapathComponent implements OnInit {
     }
   }
 
+  movePlayerTo(dir, targetIdx) {
+    this.isWalking = dir;
+    this.timer = setInterval(() => {
+      this.currentNodeIndex = this.currentNodeIndex + dir;
+      if (this.currentNodeIndex === targetIdx) {
+        this.isWalking = 0;
+        clearInterval(this.timer);
+        this.gotoUrl();
+      }
+    }, this.walkingSpeed);
+  }
+
   public nodeClick(idx) {
-    if (idx === this.currentNodeIndex) {
-      this.goToPoint(+1);
+
+    let dir = 1;
+    if (idx < this.currentNodeIndex) {
+      dir = -1;
     }
+
+    this.movePlayerTo(dir, idx);
+
     console.log('nodeClick' + idx);
+  }
+
+  public gotoUrl() {
+    console.log('gotoUrl');
   }
 
 }
