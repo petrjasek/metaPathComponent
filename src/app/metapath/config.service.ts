6896ecace80d044ca0cc25fcb17a9fbdf9c06968
Michal Hotovec <michal.hotovec@gmail.com>
2018-06-04 18:25:07 +0200

    add config service

---
+++ src/app/metapath/config.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ConfigService {
+
+  public config = null;
+
+  constructor() {
+  }
+
+  public setConfig(configVariableName) {
+    if (!window[configVariableName]) {
+      console.error('missing config');
+    }
+    this.config = window[configVariableName];
+  }
+
+  public logConfig() {
+    console.dir(this.config);
+  }
+
+  getId(): string {
+    return this.config.pathId;
+  }
+
+  getLayers() {
+    return this.config.layers;
+  }
+
+  getLayer(idx) {
+    return this.config.layers[idx];
+  }
+
+}
